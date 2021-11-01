ea282713e602194469fd533a1fa75a3536f2b9fe
douglaslimab <douglaslimab@gmail.com>
2021-11-01 18:22:49 +0000

    Move Character

--- js/OverworldMap.js
+++ js/OverworldMap.js
@@ -1,82 +1,83 @@
-class OverworldMap{
-    constructor(config){
-        this.gameObjects = config.gameObjects;
-        
-        this.lowerImage = new Image();
-        this.lowerImage.src = config.lowerSrc;
+class OverworldMap {
+  constructor(config) {
+    this.gameObjects = config.gameObjects;
 
-        this.upperImage = new Image();
-        this.upperImage.src = config.upperSrc
-    }
+    this.lowerImage = new Image();
+    this.lowerImage.src = config.lowerSrc;
 
-    drawLowerImage(ctx){
-        ctx.drawImage(this.lowerImage, 0, 0);
-    }
+    this.upperImage = new Image();
+    this.upperImage.src = config.upperSrc;
+  }
 
-    drawUpperImage(ctx){
-        ctx.drawImage(this.upperImage, 0, 0);
-    }
+  drawLowerImage(ctx) {
+    ctx.drawImage(this.lowerImage, 0, 0)
+  }
+
+  drawUpperImage(ctx) {
+    ctx.drawImage(this.upperImage, 0, 0)
+  } 
 }
 
 window.OverworldMaps = {
-    Grass:{
-        lowerSrc: "/img/grass_bg.png",
-        upperSrc: "",
-        gameObjects:{
-            doug: new GameObject({
-                x: 5,
-                y: 6,
-            }),
-            npc1: new GameObject({
-                x: 9,
-                y: 2,
-                src: "/img/npc-sprite1.png",
-            })
-        }
-    },
-    Sand:{
-        lowerSrc: "/img/sand_bg.png",
-        upperSrc: "",
-        gameObjects:{
-            doug: new GameObject({
-                x: 5,
-                y: 6,
-            }),
-            npc1: new GameObject({
-                x: 9,
-                y: 2,
-                src: "/img/npc-sprite1.png",
-            })
-        }
-    },
-    Pavement:{
-        lowerSrc: "/img/pavement_bg.png",
-        upperSrc: "",
-        gameObjects:{
-            doug: new GameObject({
-                x: 5,
-                y: 6,
-            }),
-            npc1: new GameObject({
-                x: 9,
-                y: 2,
-                src: "/img/npc-sprite1.png",
-            })
-        }
-    },
-    Water:{
-        lowerSrc: "/img/water_bg.png",
-        upperSrc: "",
-        gameObjects:{
-            doug: new GameObject({
-                x: 5,
-                y: 6,
-            }),
-            npc1: new GameObject({
-                x: 9,
-                y: 2,
-                src: "/img/npc-sprite1.png",
-            })
-        }
-    },
+  Sand: {
+    lowerSrc: "/img/sand_bg.png",
+    upperSrc: "",
+    gameObjects: {
+      hero: new Person({
+        isPlayerControlled: true,
+        x: utils.withGrid(8),
+        y: utils.withGrid(8),
+      }),
+      npc1: new Person({
+        x: utils.withGrid(12),
+        y: utils.withGrid(12),
+        src: "/img/npc-sprite1.png"
+      })
+    }
+  },
+  Grass: {
+    lowerSrc: "/img/grass_bg.png",
+    upperSrc: "",
+    gameObjects: {
+      doug: new GameObject({
+        x: utils.withGrid(8),
+        y: utils.withGrid(8),
+      }),
+      npc1: new GameObject({
+        x: utils.withGrid(12),
+        y: utils.withGrid(8),
+        src: "/img/npc-sprite1.png"
+      }),
+    }
+  },
+  Water: {
+    lowerSrc: "/img/water_bg.png",
+    upperSrc: "",
+    gameObjects: {
+      doug: new GameObject({
+        x: utils.withGrid(8),
+        y: utils.withGrid(8),
+      }),
+      npc1: new GameObject({
+        x: utils.withGrid(8),
+        y: utils.withGrid(12),
+        src: "/img/npc-sprite1.png"
+      }),
+    }
+  },
+  Pavement: {
+    lowerSrc: "/img/pavement_bg.png",
+    upperSrc: "",
+    gameObjects: {
+      doug: new GameObject({
+        x: utils.withGrid(8),
+        y: utils.withGrid(8),
+      }),
+      npc1: new GameObject({
+        x: utils.withGrid(4),
+        y: utils.withGrid(4),
+        src: "/img/npc-sprite1.png"
+      }),
+    }
+  },
 }
